83b9009b976efdd47bfa6920794960a247e726c9
lichang <2798323033>
2024-05-14 15:34:55 +0800

    update:修改样式

--- js/model.js
+++ js/model.js
@@ -1,8 +1,52 @@
 //鼠标状态 up || down
 let mouseStatus = "up"; //down
+//模态框是否展示
 let modelShow = false;
-/** 获取选中的文本 */
+
+let selelctTxt = "";
+
+/** 点击内容是否在model里 */
+const isClickModel = (e) => {
+  const modelDom = document.querySelector(".brower_helper-model_box");
+  if (!modelDom) return false;
+  return modelDom.contains(e.target);
+};
+
+/** 复制内容到剪贴板 */
+const useCopy = (content) => navigator.clipboard.writeText(content);
+
+/** 获取选中的文本并去除两边空格 */
 const getSelectedText = () => window.getSelection().toString().trim();
+
+const modelList = [
+  {
+    name: "复制",
+    handle: () => {
+      if (!selelctTxt) return;
+      return useCopy(selelctTxt);
+    },
+  },
+  {
+    name: "打开",
+    handle: () => {
+      window.open(
+        `https://www.baidu.com/s?rsv_dl=selectedsearch&wd=${selelctTxt}`
+      );
+    },
+  },
+];
+
+/** 创建模态框的每一项 */
+const createModelItem = (ModelDom) => {
+  modelList.forEach((modelItem) => {
+    const modelItemDom = document.createElement("div");
+    modelItemDom.innerText = modelItem.name;
+    modelItemDom.classList.add("brower_helper-model_item");
+    modelItemDom.addEventListener("click", () => modelItem.handle());
+    ModelDom.appendChild(modelItemDom);
+  });
+};
+
 /** 创建一个模态框 */
 const createModelDom = () => {
   //外层容器
@@ -11,6 +55,10 @@ const createModelDom = () => {
   // 设置模态框的样式
   const browerHelpeModelMain = document.createElement("div");
   browerHelpeModelMain.classList.add("brower_helper-model_main");
+  //添加模态框选项
+  createModelItem(browerHelpeModelMain);
+  let childrenLength = browerHelpeModelMain.childElementCount;
+  browerHelpeModelMain.style.gridTemplateColumns = `repeat(${childrenLength}, 1fr)`;
   browerHelperModelBox.appendChild(browerHelpeModelMain);
   document.body.appendChild(browerHelperModelBox);
 };
@@ -24,8 +72,8 @@ const showModel = (range) => {
   const rect = range.getBoundingClientRect();
   modelDom.style.display = "block";
   modelShow = true;
-  const x = rect.left - modelDom.clientWidth / 2.3;
-  const y = rect.top - modelDom.clientHeight * 1.5; // 设置偏移量
+  const x = rect.left - modelDom.clientWidth / 2.5;
+  const y = rect.top - modelDom.clientHeight * 1.3; // 设置偏移量
   modelDom.style.left = `${x}px`;
   modelDom.style.top = `${y}px`;
 };
@@ -49,9 +97,9 @@ const clickDownHandler = (event) => {
 const clickUpHandler = (e) => {
   if (mouseStatus === "up") return;
   mouseStatus = "up";
-  //获取选中的文本并去除两边空格
-  const selectedText = getSelectedText();
-  if (selectedText === "") return;
+  if (isClickModel(e)) return;
+  selelctTxt = getSelectedText();
+  if (selelctTxt === "") return;
   const range = window.getSelection().getRangeAt(0);
   showModel(range);
   //文本所在元素
